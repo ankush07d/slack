71dbab43459a5f6fc82cee7fbade402a4435e1da
Ankush Dhallod <89374868+ankush07d@users.noreply.github.com>
2024-05-09 11:48:51 +0530

    Fix: Update setup.py

--- setup.py
+++ setup.py
@@ -24,3 +24,37 @@
 # 	install_requires=[str(ir.req) for ir in requirements],
 # 	dependency_links=[str(ir._link) for ir in requirements if ir._link]
 # )
+
+# -------------------updated code-----------------------------------------
+
+from setuptools import setup, find_packages
+import re, ast
+
+# Get version from __version__ variable in slack/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('slack/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+# Parse requirements from 'requirements.txt'
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='slack',
+    version=version,
+    description='Frappe and ERPNext Notification Integration',
+    author='Bai Web and Mobile Labs',
+    author_email='ccfiel@bai.ph',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=requirements,
+)
+
+
+
+
+
+
